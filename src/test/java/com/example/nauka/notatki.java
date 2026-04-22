926f6762741b9221e9e83e480912f89a2bbdd41a
czarny-fred <antoniszydlo24@gmail.com>
2026-04-22 13:53:44 +0200

    Delete src directory

--- src/test/java/com/example/nauka/notatki.java
+++
@@ -1,9 +0,0 @@
-package com.example.nauka;
-
-public class notatki {
-    byte Age = 30;
-    int viewsCounter = 123_456_789;
-    long bigNumbers = 3_123_456_789L;
-    float price = 10.99F;
-    double priceBig = 100.99;
-}
